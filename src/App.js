d92c8276fabca955597f603f5299798655aee2bd
Rishi Challa <rishichalla@Rishis-MacBook-Pro.local>
2022-09-18 22:10:42 -0400

    cleaning up

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Authentication/Login";
 import Signup from "./components/Authentication/Signup";
-import Home from "./components/Home";
 import PrivateRoute from "./components/Authentication/PrivateRoute";
 import { AuthProvider } from "./contexts/AuthContext";
 import { FirestoreProvider } from "./contexts/FirestoreContext";
-import Profile from "./components/Profile/Profile";
 import ForgotPassword from "./components/Authentication/ForgotPassword";
-import Friends from "./components/Friends";
 import CryptoPortfolio from "./components/Portfolio/CryptoPortfolio";
 import { CryptoProvider } from "./contexts/CryptoContext";
 
