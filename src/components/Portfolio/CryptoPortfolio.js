59021e5f97540a0030e3e984373bc8ad158e74dd
rchalla67 <rishichalla67@gmail.com>
2023-01-29 12:14:44 -0500

    moved privacy toggle to right

--- src/components/Portfolio/CryptoPortfolio.js
+++ src/components/Portfolio/CryptoPortfolio.js
@@ -5,6 +5,7 @@ import React, {
   useCallback,
   Fragment,
 } from "react";
+import { motion } from "framer-motion";
 import { Dialog, Transition } from "@headlessui/react";
 import { useCryptoOracle } from "../../contexts/CryptoContext";
 import { useFirestore } from "../../contexts/FirestoreContext";
@@ -240,20 +241,6 @@ export default function CryptoPortfolio() {
     else return "Invalid Input";
   }
 
-  function maskArrayOfObjects(input) {
-    if (Array.isArray(input)) {
-      input.map(function (item) {
-        if (typeof item.value === "number")
-          item.value = item.value.toString().replace(/[\d\.]/g, "*");
-        else if (typeof item.value === "string")
-          item.value = item.value.replace(/[\d\.]/g, "*");
-      });
-      return input;
-    } else {
-      return "Invalid Input";
-    }
-  }
-
   function formatDate(date) {
     let formattedDate;
     let momentDate = moment(date, "YYYY-MM-DD HH:mm:ss"); //parse the date in this format
@@ -285,14 +272,11 @@ export default function CryptoPortfolio() {
     return formattedDate;
   }
 
-  // function calculatePnl(data) {
-  //   return (
-  //     ((data[data.length - 1].value - data[0].value) /
-  //       data[data.length - 1].value) *
-  //     100
-  //   ).toFixed(2);
-  // }
-
+  // const portfolioValueRef = useRef(null);
+  // useEffect(() => {
+  //   portfolioValueRef.current.classList.add("animate-pulse");
+  // }, []);
+  const constraintsRef = useRef(null);
   if (!activeUser.id) {
     return (
       <div className="h-full bg-gradient-to-r from-indigo-900 via-purple-900 to-indigo-900"></div>
@@ -352,7 +336,6 @@ export default function CryptoPortfolio() {
                             <form
                               className={`h-full p-4 md:mt-8 mx-8`}
                               action="#"
-                              // onSubmit={}
                             >
                               <div className="text-white rounded-md shadow-sm -space-y-px">
                                 <h3 className="flex justify-center text-2xl font-bold text-sky-400">{`Edit ${
@@ -417,7 +400,6 @@ export default function CryptoPortfolio() {
                                   <button
                                     type="delete"
                                     onClick={() => {
-                                      // setShowForm("invisible");
                                       removePosition(selectedPosition);
                                       setSuccessMessage(
                                         "Successfully removed " +
@@ -440,7 +422,6 @@ export default function CryptoPortfolio() {
                                     <button
                                       type="submit"
                                       onClick={(e) => {
-                                        // setShowForm("invisible");
                                         updateSelectedPosition(e);
                                       }}
                                       className={`${
@@ -490,61 +471,11 @@ export default function CryptoPortfolio() {
                   </div>
                 </div>
               )}
-              <div className="flex justify-center pl-16 sm:pl-24 pb-2">
+              <div className="flex justify-center pl-20 sm:pl-24 pb-2">
                 <h3 className="pt-3 sm:pt-1 flex grow flex-col text-4xl sm:text-5xl leading-6 font-small sm:font-medium">
                   <div
-                    className={`flex justify-center pb-2 ${
-                      privacyFilter ? "hidden" : ""
-                    }`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="#0092ff"
-                      className="w-6 h-6 hover:cursor-pointer"
-                      onClick={() => {
-                        setPrivacyFilter(!privacyFilter);
-                      }}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                      />
-                    </svg>
-                  </div>
-                  <div
-                    className={`flex justify-center ${
-                      privacyFilter ? "" : "hidden"
-                    }`}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="#0092ff"
-                      className="w-6 h-6 hover:cursor-pointer"
-                      onClick={() => {
-                        setPrivacyFilter(!privacyFilter);
-                      }}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                      />
-                    </svg>
-                  </div>
-                  <div
-                    className={`${privacyFilter ? "hidden" : "pt-2 sm:pt-0"}`}
+                    className={`${privacyFilter ? "hidden" : "pt-8 sm:pt-0"}`}
+                    // ref={portfolioValueRef}
                   >
                     {`$${addCommaToNumberString(portfolioValue)}`}
                   </div>
@@ -594,8 +525,57 @@ export default function CryptoPortfolio() {
                 </h3>
 
                 <div
-                  className={`pl-7 pt-3 ${refreshAvailable ? "" : "disabled"}`}
+                  className={`pt-3 flex flex-row ${
+                    refreshAvailable ? "" : "disabled"
+                  }`}
                 >
+                  <div className={`pr-2 ${privacyFilter ? "" : "hidden"}`}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="#0092ff"
+                      className="w-6 h-6 hover:cursor-pointer"
+                      onClick={() => {
+                        setPrivacyFilter(!privacyFilter);
+                      }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  </div>
+                  <div
+                    className={`flex justify-center pb-2 pr-2 ${
+                      privacyFilter ? "hidden" : ""
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="#0092ff"
+                      className="w-6 h-6 hover:cursor-pointer"
+                      onClick={() => {
+                        setPrivacyFilter(!privacyFilter);
+                      }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  </div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -680,7 +660,6 @@ export default function CryptoPortfolio() {
                           <LineChart data={filteredPortfolioValueHistory}>
                             <XAxis hide={true} dataKey="date" />
                             <YAxis
-                              // tick={!privacyFilter}
                               hide={privacyFilter}
                               axisLine={false}
                               dataKey="value"
@@ -690,7 +669,6 @@ export default function CryptoPortfolio() {
                               }
                               // tick={{ fontSize: "0.rem" }}
                               domain={[
-                                // filterDataByRange(portfolioValueHistory, currentChartDateRange)[0].value
                                 Math.round(
                                   parseInt(
                                     (findMinValue(
@@ -711,12 +689,6 @@ export default function CryptoPortfolio() {
                                 ), // upper bound
                               ]}
                             />
-                            {/* <Tooltip
-                              style={{ color: "red" }}
-                              contentStyle={{ backgroundColor: "#000000" }}
-                              itemStyle={{ color: "#FFFFFF" }}
-                            /> */}
-
                             <Tooltip
                               style={{ color: "red" }}
                               content={({ payload }) => {
@@ -793,7 +765,6 @@ export default function CryptoPortfolio() {
                   {tabIndex === 1 && (
                     <>
                       <div className="flex pb-2 border border-gray-200">
-                        {/* <a className="pl-3 text-white-500 text-2xl">-</a> */}
                         <h3 className="pl-3 pt-2 text-xl leading-6 text-sky-500 font-medium">
                           Crypto
                         </h3>
@@ -870,7 +841,6 @@ export default function CryptoPortfolio() {
                     onSubmit={handleSubmit}
                   >
                     <div className="pt-4 sm:px-6">
-                      {/* <h3 className="font-semibold pb-2"></h3> */}
                       <input
                         id="search"
                         name="search"
@@ -886,7 +856,6 @@ export default function CryptoPortfolio() {
                   <div className="px-10 overflow-y-auto h-48 border-b">
                     {searchResults &&
                       searchResults.map((result) => {
-                        // console.log(result)
                         return (
                           <div
                             key={`${result.api_symbol}`}
@@ -1009,17 +978,6 @@ export default function CryptoPortfolio() {
                 </div>
                 <div className="px-10 py-10 " action="#">
                   <div className="flex justify-center pt-4 sm:px-6">
-                    {/* <h3 className="font-semibold pb-2"></h3> */}
-
-                    {/* <input
-                      id="search"
-                      name="search"
-                      autoComplete="off"
-                      ref={portfolioNameRef}
-                      required
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Enter Portfolio Name"
-                    /> */}
                     <button
                       className="bg-sky-500 hover:bg-sky-700 text-black font-bold py-2 px-4 rounded"
                       onClick={() => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      minWidth: {
-        "75%": "75%",
-        "90%": "90%",
-        "95%": "95%",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    minWidth: {
+      "75%": "75%",
+      "90%": "90%",
+      "95%": "95%",
+    },
+    minHeight: {
+      "98vh": "98vh",
+      "90%": "90%",
+    },
+    extend: {
+      animation: {
+        spin: "spin 1s linear infinite",
       },
-      minHeight: {
-        "98vh": "98vh",
-        "90%": "90%",
+      animation: {
+        bounce: "bounce 1s ease-in-out infinite",
+        "bounce-dot": "bounce 1s ease-in-out infinite",
       },
-      extend: {
-        animation: {
-          'spin': 'spin 1s linear infinite',
+      "example-container": {
+        "& > div": {
+          bg: "white",
+          "border-radius": "30px",
+          width: "150px",
+          height: "150px",
+          position: "absolute",
+          top: "calc(50% - 150px / 2)",
+          left: "calc(50% - 150px / 2)",
         },
-        animation: {
-          'bounce': 'bounce 1s ease-in-out infinite',
-          'bounce-dot': 'bounce 1s ease-in-out infinite',
+        "& .drag-area": {
+          opacity: ".2",
+          background: "white",
+          position: "absolute",
+          width: "300px",
+          height: "300px",
+          "border-radius": "30px",
+          top: "calc(50% - 150px)",
+          left: "calc(50% - 150px)",
         },
       },
     },
-    plugins: [require("@tailwindcss/forms")],
-  };
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
