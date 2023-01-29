e928bc12f2cd35db19e5ba9f4d2b58c91a56bedb
rchalla67 <rishichalla67@gmail.com>
2023-01-29 08:28:31 -0500

    added privacy feature

--- src/components/Portfolio/CryptoPortfolio.js
+++ src/components/Portfolio/CryptoPortfolio.js
@@ -39,6 +39,7 @@ export default function CryptoPortfolio() {
   const [selectedPosition, setSelectedPosition] = useState();
   const [successMessage, setSuccessMessage] = useState("");
   const [showForm, setShowForm] = useState("invisible");
+  const [privacyFilter, setPrivacyFilter] = useState(false);
 
   const [disable, setDisable] = useState(true);
   const [open, setOpen] = useState(true);
@@ -229,6 +230,10 @@ export default function CryptoPortfolio() {
     } else {
       setDisable(false);
     }
+  }
+
+  function maskNumber(number) {
+    return number.toString().replace(/[\d\.]/g, "*");
   }
 
   // function calculatePnl(data) {
@@ -438,7 +443,63 @@ export default function CryptoPortfolio() {
               )}
               <div className="flex justify-center pl-16 sm:pl-24 pb-2">
                 <h3 className="pt-3 sm:pt-1 flex grow flex-col text-4xl sm:text-5xl leading-6 font-small sm:font-medium">
-                  {`$${addCommaToNumberString(portfolioValue)}`}
+                  <div
+                    className={`flex justify-center pb-2 ${
+                      privacyFilter ? "hidden" : ""
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="#0092ff"
+                      className="w-6 h-6 hover:cursor-pointer"
+                      onClick={() => {
+                        setPrivacyFilter(!privacyFilter);
+                      }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                      />
+                    </svg>
+                  </div>
+                  <div
+                    className={`flex justify-center ${
+                      privacyFilter ? "" : "hidden"
+                    }`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="#0092ff"
+                      className="w-6 h-6 hover:cursor-pointer"
+                      onClick={() => {
+                        setPrivacyFilter(!privacyFilter);
+                      }}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                      />
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                      />
+                    </svg>
+                  </div>
+                  <div className={`${privacyFilter ? "hidden" : "pb-2"}`}>
+                    {`$${addCommaToNumberString(portfolioValue)}`}
+                  </div>
+                  <div
+                    className={`${privacyFilter ? "" : "hidden"}`}
+                  >{`$${maskNumber(portfolioValue)}`}</div>
                   {filteredPortfolioValueHistory.length > 0 && (
                     <div
                       className={`pl-2 pt-4 text-lg sm:pl-3 pb-1 leading-6 ${
