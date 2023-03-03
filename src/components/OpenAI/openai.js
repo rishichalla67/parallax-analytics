212d0edbd2076209141a00737ce05797e7ccfba8
rchalla67 <rishichalla67@gmail.com>
2023-03-02 19:04:07 -0500

    updated openAI model

--- src/components/OpenAI/openai.js
+++ src/components/OpenAI/openai.js
@@ -7,6 +7,13 @@ const OpenAI = () => {
   const [outputText, setOutputText] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [unformattedResponse, setUnformattedResponse] = useState("");
+  const [selectedOption, setSelectedOption] = useState({
+    category: "Finance",
+    prompt:
+      "You are a financial expert in all frontiers of finance, digital and traditional.",
+  });
 
   const handleChange = (event) => {
     setInputText(event.target.value);
@@ -43,6 +50,29 @@ const OpenAI = () => {
     </svg>
   );
 
+  const prompts = [
+    {
+      category: "Finance",
+      prompt:
+        "You are a financial expert in all frontiers of finance, digital and traditional.",
+    },
+    {
+      category: "Software",
+      prompt:
+        "You are a software expert who can solve complex coding problems with ease.",
+    },
+    {
+      category: "Marketing",
+      prompt:
+        "You are a marketing expert who knows how to connect with audiences and create successful campaigns.",
+    },
+    {
+      category: "Life",
+      prompt:
+        "You are a life expert who can provide guidance on personal growth, wellness, and relationships.",
+    },
+  ];
+
   const formatResponse = (text) => {
     let formattedText = text;
 
@@ -66,20 +96,27 @@ const OpenAI = () => {
       const response = await axios.post(
         "https://api.openai.com/v1/chat/completions",
         {
-          messages: [{ role: "user", content: inputText }],
+          messages: [
+            {
+              role: "system",
+              content: `${selectedOption.prompt}`,
+            },
+            { role: "user", content: inputText },
+          ],
           model: "gpt-3.5-turbo",
         },
         {
           headers: {
             "Access-Control-Allow-Origin": "*",
             "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+            Authorization: `Bearer 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`,
           },
         }
       );
       const formattedResponse = formatResponse(
         response.data.choices[0].message.content
       );
+      setUnformattedResponse(response.data.choices[0].message.content);
       setOutputText(formattedResponse);
     } catch (error) {
       setError(error);
@@ -92,9 +129,67 @@ const OpenAI = () => {
       <Nav />
       <div className="p-4 min-h-full h-screen bg-gradient-to-r from-indigo-900 via-purple-900 to-indigo-900">
         <h1 className="text-2xl font-bold text-white mb-4">Ask Anything...</h1>
+        <div className="relative inline-block text-left">
+          <div className="pb-2">
+            <span className="rounded-md shadow-sm">
+              <button
+                type="button"
+                className="inline-flex text-opacity-70 justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-slate-600 text-sm font-medium text-white hover:bg-slate-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+                id="options-menu"
+                aria-haspopup="true"
+                aria-expanded="true"
+                onClick={() => setIsOpen(!isOpen)}
+              >
+                <span className="mr-2">
+                  Selected Expert: <b>{selectedOption.category}</b>
+                </span>
+                <svg
+                  className="-mr-1 ml-2 h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.293 7.293a1 1 0 011.414 0L10 9.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </button>
+            </span>
+          </div>
+
+          {isOpen && (
+            <div
+              className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-slate-600 text-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="options-menu"
+            >
+              <div className="py-1" role="none">
+                {prompts.map((option) => (
+                  <div
+                    key={option.category}
+                    className="flex items-center px-4 py-2 text-sm text-white hover:bg-gray-100 hover:text-gray-900 cursor-pointer"
+                    role="menuitem"
+                    onClick={() => {
+                      setSelectedOption(option);
+                      setIsOpen(false);
+                    }}
+                  >
+                    <div className="rounded-full bg-gray-200 mr-2 h-2 w-2"></div>
+                    <span className="flex-1">{option.category}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+
         <form onSubmit={handleSubmit}>
           <textarea
-            className="w-full p-2 rounded-lg bg-gray-100 focus:outline-none focus:bg-white"
+            className="w-full p-2 rounded-lg text-white bg-slate-600 focus:outline-none "
             value={inputText}
             onChange={handleChange}
             onKeyDown={(e) => {
@@ -140,7 +235,7 @@ const OpenAI = () => {
               />
               <button
                 className="hover:bg-gradient-to-r from-indigo-900 via-indigo-3500 to-indigo-900 bg-slate-500 w-full top-0 right-0 p-2 text-white"
-                onClick={() => copyToClipboard(outputText)}
+                onClick={() => copyToClipboard(unformattedResponse)}
               >
                 Copy
               </button>
