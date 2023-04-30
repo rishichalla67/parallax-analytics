c9029899fc071b71609b4c80f19697c9a9528989
rchalla67 <rishichalla67@gmail.com>
2023-04-30 12:34:09 -0400

    updated settings selection

--- src/components/Portfolio/PortfolioSettings.js
+++ src/components/Portfolio/PortfolioSettings.js
@@ -10,6 +10,14 @@ function PortfolioSettings({ sortedPositions, setShowSettingsModal }) {
   const [sortDescending, setSortDescending] = useState(
     sortedPositions[0].value < sortedPositions[1].value
   );
+  const positionOptions = ["Crypto", "Quantity", "Value"];
+  const analyticsOptions = [
+    "Position 24Hr Δ",
+    "Symbol",
+    "Position PnL",
+    "Current Price",
+    "Average Price",
+  ];
 
   const handleSaveSettings = () => {
     // Save the selected settings and close the modal
@@ -26,30 +34,65 @@ function PortfolioSettings({ sortedPositions, setShowSettingsModal }) {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-indigo-900 rounded-lg shadow-lg p-6 w-[70dvh] flex flex-col">
         <h2 className="text-lg font-medium mb-4">Portfolio Settings</h2>
-        <div className="flex items-center justify-center mb-4">
-          <span>Sort positions by:</span>
-          <div className="flex items-center ml-4">
-            <button
-              className={`${
-                sortDescending ? "bg-red-600" : "bg-gray-200"
-              } inline-flex items-center justify-center h-6 w-6 rounded-full`}
-              onClick={() => setSortDescending(true)}
+
+        <div className="mb-6">
+          <h3 className="text-base font-semibold underline mb-2">Positions</h3>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="positionSortColumn" className="font-medium ">
+              Choose Default Column to Sort
+            </label>
+            <select
+              id="positionSortColumn"
+              className="rounded border-gray-300 bg-black shadow-sm"
             >
-              <ArrowDownIcon className="w-4 h-4 text-black" />
-            </button>
-            <span className="mx-2 text-gray-400">Desc</span>
-            <button
-              className={`${
-                sortDescending ? "bg-gray-200" : "bg-green-600"
-              } inline-flex items-center justify-center h-6 w-6 rounded-full`}
-              onClick={() => setSortDescending(false)}
+              {positionOptions.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="positionSortDirection" className="font-medium">
+              Choose Default Sort Direction
+            </label>
+            <select
+              id="positionSortDirection"
+              className="rounded border-gray-300 bg-black shadow-sm"
             >
-              <ArrowUpIcon className="w-4 h-4 text-black" />
-            </button>
-            <span className="mx-2 text-gray-400">Asc</span>
+              <option value="ascending">Ascending</option>
+              <option value="descending">Descending</option>
+            </select>
           </div>
         </div>
-        <div className="flex-grow"></div>
+
+        <div className="mb-6">
+          <h3 className="text-base font-semibold underline mb-2">Analytics</h3>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="analyticsSortColumn" className="font-medium">
+              Choose Default Column to Sort
+            </label>
+            <select
+              id="analyticsSortColumn"
+              className="rounded border-gray-300 bg-black shadow-sm"
+            >
+              {analyticsOptions.map((option, index) => (
+                <option key={index} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <label htmlFor="analyticsSortDirection" className="font-medium">
+              Choose Default Sort Direction
+            </label>
+            <select
+              id="analyticsSortDirection"
+              className="rounded border-gray-300 bg-black shadow-sm"
+            >
+              <option value="ascending">Ascending</option>
+              <option value="descending">Descending</option>
+            </select>
+          </div>
+        </div>
+
         <div className="flex justify-center space-x-4">
           <button
             className="bg-gray-200 text-gray-700 px-4 py-2 rounded"
