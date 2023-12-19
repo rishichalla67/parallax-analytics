a6b1df94d95ca01809dbc39da234d690472de479
rchalla67 <rishichalla67@gmail.com>
2023-12-18 20:28:07 -0500

    no more server calls rn :(

--- src/contexts/CryptoContext.js
+++ src/contexts/CryptoContext.js
@@ -89,10 +89,10 @@ export function CryptoProvider({ children }) {
     });
 
     fetch(
-      `${serverURL}/symbolData?symbols=${positionSymbolList}`
-      // `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${positionSymbolList.join(
-      //   ","
-      // )}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y`
+      // `${serverURL}/symbolData?symbols=${positionSymbolList}`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${positionSymbolList.join(
+        ","
+      )}&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y`
     )
       .then((response) => response.json())
       .then((searchResponse) => {
